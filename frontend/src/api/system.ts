949069bcc1fe5862c861fdc7cd80f89d83e5279a
AlexaZhou <AlexaZhou@163.com>
2026-03-10 02:17:49 +0800

    fix

--- frontend/src/api/system.ts
+++ frontend/src/api/system.ts
@@ -1,7 +1,7 @@
 import request from '@/utils/request';
 
 export function welcome(): Promise<any> {
-    return request.get('/');
+    return request.get('/status.json');
 }
 
 export function status(): Promise<any> {
